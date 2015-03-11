e79d866090aa30689a1c56a4685dc6bab5a95de0
U-ASIAPACIFIC\tangyunl <tangyunl@TANGYUNL5.asiapacific.hpqcorp.net>
2015-03-11 22:20:51 +0800

    check in code

--- config/config.php
+++ config/config.php
@@ -1,1 +1,62 @@
-//网站配置
+<?php return array (
+  'logs' => 
+  array (
+    'path' => 'logs/log',
+    'type' => 'file',
+  ),
+  'DB' => 
+  array (
+    'type' => 'mysqli',
+    'tablePre' => 'kz_',
+    'read' => 
+    array (
+      0 => 
+      array (
+        'host' => 'localhost:3306',
+        'user' => 'root',
+        'passwd' => 'baihuawei',
+        'name' => 'bhw_pc',
+      ),
+    ),
+    'write' => 
+    array (
+      'host' => 'localhost:3306',
+      'user' => 'root',
+      'passwd' => 'baihuawei',
+      'name' => 'bhw_pc',
+    ),
+  ),
+  'interceptor' => 
+  array (
+    0 => 'themeroute@onCreateController',
+    1 => 'layoutroute@onCreateView',
+  ),
+  'langPath' => 'language',
+  'viewPath' => 'views',
+  'skinPath' => 'skin',
+  'classes' => 'classes.*',
+  'rewriteRule' => 'pathinfo',
+  'theme' => 
+  array (
+    'pc' => 'baihua',
+    'mobile' => 'mobile',
+  ),
+  'skin' => 
+  array (
+    'pc' => 'default',
+    'mobile' => '',
+  ),
+  'timezone' => 'Etc/GMT-8',
+  'upload' => 'upload',
+  'dbbackup' => 'backup/database',
+  'safe' => 'cookie',
+  'safeLevel' => 'none',
+  'lang' => 'zh_sc',
+  'debug' => false,
+  'configExt' => 
+  array (
+    'site_config' => 'config/site_config.php',
+    '<action:.*>' => 'site/<action>'
+  ),
+  'encryptKey' => 'ca2ad7e1c1b0688e172ea8011c473435',
+)?>
